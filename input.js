57757d6ee2e155bcb7586ce98ec4cc928c44b97b
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-24 22:41:54 -0700

    Implement WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function() {
+
+// Stores the active TCP connection object
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +19,16 @@ const handleUserInput = function(key) {
   // If key is " Ctrl+ c" exit the program
   if (key === '\u0003') {
     process.exit();
+  
+  // Implement WASD Movement
+  } else if (key === "w" || key === "W") {
+    connection.write("Move: up");
+  } else if (key === "a" || key === "A") {
+    connection.write("Move: left");
+  } else if (key === "s" || key === "S") {
+    connection.write("Move: down");
+  } else if (key === "d" || key === "D") {
+    connection.write("Move: right");
   }
 };
 
