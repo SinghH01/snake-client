6d05050acfabd9465132f2f80c2b4d83009b443b
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-24 21:57:04 -0700

    Setup Input and handle user input

--- play.js
+++ play.js
@@ -3,3 +3,24 @@ const connect = require("./client");
 
 console.log('Connection ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  // Event Listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // If key is " Ctrl+ c" exit the program
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
