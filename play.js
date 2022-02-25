4005a1c5c6bb895e37ec94bf9325ff9a8f9ee245
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-24 22:20:41 -0700

    Import user input handling function

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 // Import connect function from client module
 const connect = require("./client");
 
+// Import function to handle input
+const setupInput = require("./input");
+
 console.log('Connection ...');
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  // Event Listener for stdin
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // If key is " Ctrl+ c" exit the program
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 setupInput();
